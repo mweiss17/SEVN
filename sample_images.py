f61a0670651cbf65d924d920f318ce511f55cf30
Martin weiss <martin.clyde.weiss@gmail.com>
2019-05-01 18:11:42 -0400

    cleaning code

--- sample_images.py
+++ sample_images.py
@@ -1,10 +1,8 @@
 import os
 from shutil import copyfile
-os.chdir("crops")
-for fname in os.listdir():
+
+for fname in os.listdir("data/crops"):
   s_fname = fname.split("_")
-  if fname == ".DS_Store":
-    continue
   print(fname)
   if int(s_fname[3].split(".")[0]) % 10 == 0 and fname.startswith("camera_2"):
-    copyfile(fname, "../10x_reduction/" + fname)
+    copyfile(fname, "data/10x_reduction/" + fname)
