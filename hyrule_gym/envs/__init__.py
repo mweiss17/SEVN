67ebebec149e15ac5f75b2a1f0e2beaf62e8ffad
mweiss17 <martin.weiss@mail.mcgill.ca>
2019-07-06 01:21:06 -0400

    added env

--- hyrule_gym/envs/__init__.py
+++ hyrule_gym/envs/__init__.py
@@ -1,3 +1,4 @@
 from hyrule_gym.envs.hyrule_base import HyruleBase
 from hyrule_gym.envs.hyrule_pacman import HyrulePacman
+from hyrule_gym.envs.hyrule_decreasing_reward import HyruleDecreasingReward
 from hyrule_gym.envs.hyrule_play import HyrulePlay
