448e6b8e785d688cc660d78d130f68b877e06071
mweiss17 <martin.weiss@mail.mcgill.ca>
2019-07-22 12:45:43 -0400

    trying to get singularity to work

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='SEVN_gym',
     version='1.0',
     install_requires=[
-        'gym>=0.2.3', 'pybullet>=1.9.4', 'torch', 'scipy', "tqdm", "matplotlib", "numpy", "networkx", \
+        'gym>=0.2.3', 'pybullet>=1.9.4', 'scipy', "tqdm", "matplotlib", "numpy", "networkx", \
         'pygame', 'h5py', 'tables'
     ])
